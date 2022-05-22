faf00cf49bb2329c6cec08bc6a1d68eca40afdc1
Alper Arslan <61477624+alpacabyte@users.noreply.github.com>
2022-05-22 14:21:03 +0300

    Users page complete

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { Text, View, StyleSheet, FlatList, SafeAreaView, TouchableOpacity} from 'react-native';
 import Constants from 'expo-constants';
 
 // You can import from local files
@@ -8,22 +8,57 @@ import AssetExample from './components/AssetExample';
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      
 
-    </View>
+
+export default function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users => {
+      setUsers(users);
+    })
+  }, []);
+
+  console.log(users);
+
+  return (
+    <SafeAreaView style={styles.container}>
+     <View style = {{borderBottomWidth : 2, marginBottom: 15}}>
+        <Text style = {styles.userHeader}> Users</Text> 
+        </View>
+      <FlatList
+      data = {users}
+      keyExtractor = {({id}, index) => id}
+      renderItem = {({item}) => (<TouchableOpacity onPress = {()=>{
+        console.log(item.id);
+      }}> 
+        <View style = {{borderBottomWidth : 1, marginBottom: 3}}>
+        <Text style = {styles.userText}> {item.name}</Text> 
+        </View>
+      </TouchableOpacity>)}/>
+    </SafeAreaView>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     paddingTop: Constants.statusBarHeight,
+    marginTop: 50,
     backgroundColor: '#ecf0f1',
     padding: 8,
+  },
+  userHeader: {
+    fontSize: 30,
+    fontWeight: "500",
+  },
+  userText: {
+    fontSize: 26,
+    fontWeight: "200",
   },
   paragraph: {
     margin: 24,
